44409a12c4dd2c28910590dbb01eea4f8ed75c48
BrunoHSL007 <brunohenrique.laier28@gmail.com>
2024-07-10 23:21:17 -0300

    Final project after NLW

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -4,6 +4,8 @@ import { InviteGuestsModal } from './invite-guests-modal'
 import { ConfirmTripModal } from './confirm-trip-modal'
 import { DestinationAndDateStep } from './steps/destination-and-date-step'
 import { InviteGuestsStep } from './steps/invite-guests-step'
+import { DateRange } from 'react-day-picker'
+import { api } from '../../lib/axios'
 
 export function CreateTripPage() {
     const navigate = useNavigate()
@@ -11,6 +13,11 @@ export function CreateTripPage() {
     const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false)
     const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false)
     const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(false)
+
+    const [destination, setDestination ] = useState('')
+    const [ownerName, setOwnerName ] = useState('')
+    const [ownerEmail, setOwnerEmail ] = useState('')
+    const [eventStartAndEndDates,setEventStartAndEndDates] = useState<DateRange | undefined>()
 
     const [emailsToInvite, setEmailsToInvite] = useState([
         'email.teste@gmail.com'
@@ -40,10 +47,37 @@ export function CreateTripPage() {
         setIsConfirmTripModalOpen(false)
     }
 
-    function createTrip(event: FormEvent<HTMLFormElement>) {
+    async function createTrip(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
 
-        navigate('/trips/123')
+        if(!destination){
+            return
+        }
+
+        if(!eventStartAndEndDates?.from || !eventStartAndEndDates?.to){
+            return
+        }
+
+        if(emailsToInvite.length === 0 ){
+            return
+        }
+
+        if(!ownerName || !ownerEmail){
+            return
+        }
+
+        const response = await api.post('/trips', {
+            destination,
+            starts_at: eventStartAndEndDates?.from,
+            ends_at: eventStartAndEndDates?.to,
+            emails_to_invite: emailsToInvite,
+            owner_name: ownerName,
+            owner_email: ownerEmail
+        })
+
+        const { tripId } = response.data
+
+        navigate(`/trips/${tripId}`)
     }
 
     function addNewEmailToInvite(event: FormEvent<HTMLFormElement>) {
@@ -88,6 +122,9 @@ export function CreateTripPage() {
                         closeGuestsInput={closeGuestsInput}
                         isGuestsInputOpen={isGuestsInputOpen}
                         openGuestsInput={openGuestsInput}
+                        setDestination={setDestination}
+                        setEventStartAndEndDates={setEventStartAndEndDates}
+                        eventStartAndEndDates={eventStartAndEndDates}
                     />
 
                     {isGuestsInputOpen && (
@@ -119,6 +156,8 @@ export function CreateTripPage() {
                 <ConfirmTripModal
                     closeConfirmTripModal={closeConfirmTripModal}
                     createTrip={createTrip}
+                    setOwnerName={setOwnerName}
+                    setOwnerEmail={setOwnerEmail}
                 />
             )}
 
--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -1,31 +1,48 @@
 import { CircleDashed, CircleCheck, UserCog } from "lucide-react";
 import { Button } from "../../components/button";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+
+interface Participant {
+    id: string
+    name: string | null
+    email: string
+    is_confirmed: boolean
+}
 
 export function Guests() {
+    const { tripId } = useParams()
+    const [participants, setParticipants] = useState<Participant[]>([])
+
+    useEffect(() => {
+        api.get(`/trips/${tripId}/participants`).then(response => setParticipants(response.data.participants))
+    }, [tripId])
+
     return (
         <div className="space-y-6">
             <h2 className="font-semibold text-xl">Convidados</h2>
 
             <div className="space-y-5">
-                <div className="flex items-center justify-between gap-4">
-                    <div className="space-y-1.5">
-                        <span className="block font-medium text-zinc-100">Jessica White</span>
-                        <span className="block text-xs text-zinc-400 truncate">
-                            jessica.white@yahoo.com
-                        </span>
-                    </div>
-                    <CircleDashed className="size-5 text-zinc-400 shrink-0" />
-                </div>
-
-                <div className="flex items-center justify-between gap-4">
-                    <div className="space-y-1.5">
-                        <span className="block font-medium text-zinc-100">Dr. Rita Pacocha</span>
-                        <span className="block text-xs text-zinc-400 truncate">
-                            lacy.stiedemann@gmail.com
-                        </span>
-                    </div>
-                    <CircleCheck className="size-5 text-lime-400 shrink-0" />
-                </div>
+                {
+                    participants.map((participant, index) => {
+                        return (
+                            <div key={participant.id} className="flex items-center justify-between gap-4">
+                                <div className="space-y-1.5">
+                                    <span className="block font-medium text-zinc-100">{participant.name ?? `Convidado ${index}`}</span>
+                                    <span className="block text-xs text-zinc-400 truncate">
+                                        {participant.email}
+                                    </span>
+                                </div>
+                                { participant.is_confirmed ? (
+                                    <CircleCheck className="size-5 text-lime-400 shrink-0" />
+                                ) : (
+                                    <CircleDashed className="size-5 text-zinc-400 shrink-0" />
+                                )}
+                            </div>
+                        )
+                    })
+                }
             </div>
 
             <Button variant="secondary" size="full">
